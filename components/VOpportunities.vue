0ab63acf24243fc2c210b056a43eb6ca07f85678
Maksim Nikulin <nikulin-max04-05@yandex.ru>
2024-05-09 01:25:19 +0300

    second commit

---
+++ components/VOpportunities.vue
@@ -0,0 +1,163 @@
+<script setup>
+// import { onMounted, ref } from "vue";
+// let myswiper = ref(null);
+// onMounted(() => {
+//   myswiper.value = new Swiper(".swiper-container", {
+//     loop: true,
+//     slidesPerView: 1,
+//     mobileFirst: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       type: "bullets",
+//       clickable: true,
+//     },
+//   });
+// });
+import { register } from "swiper/element/bundle";
+register();
+</script>
+
+<template>
+  <section class="opportunities">
+    <h2>Возможности</h2>
+    <!-- <div class="swiper-container mobile">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <div class="slider-card" id="card-1">
+            <img src="/sms.png" alt="" />
+            <h3>Общение с классом</h3>
+            <p>
+              Видеосвязь <br />
+              Чат с классом
+            </p>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="slider-card" id="card-2">
+            <img src="/komp.png" alt="" />
+            <h3>Проведение занятий</h3>
+            <p>
+              Презентация файлов <br />
+              Демонстрация экрана
+            </p>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="slider-card" id="card-1">
+            <img src="/point.png" alt="" />
+            <h3>Посещаемость</h3>
+            <p>
+              Автоматическое <br />
+              отслеживание <br />
+              присутствующих
+            </p>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="slider-card" id="card-2">
+            <img src="/time.png" alt="" />
+            <h3>История занятий</h3>
+            <p>Материалы урока</p>
+          </div>
+        </div>
+      </div>
+      <div class="swiper-pagination"></div>
+    </div> -->
+
+    <swiper-container
+      slides-per-view="1"
+      loop="true"
+      :pagination="{
+        type: bullets,
+      }"
+      class="mobile"
+    >
+      <swiper-slide>
+        <div class="slider-card" id="card-1">
+          <img src="/sms.png" alt="" />
+          <h3>Общение с классом</h3>
+          <p>
+            Видеосвязь <br />
+            Чат с классом
+          </p>
+        </div>
+      </swiper-slide>
+
+      <swiper-slide>
+        <div class="slider-card" id="card-2">
+          <img src="/komp.png" alt="" />
+          <h3>Проведение занятий</h3>
+          <p>
+            Презентация файлов <br />
+            Демонстрация экрана
+          </p>
+        </div>
+      </swiper-slide>
+
+      <swiper-slide>
+        <div class="slider-card" id="card-1">
+          <img src="/point.png" alt="" />
+          <h3>Посещаемость</h3>
+          <p>
+            Автоматическое <br />
+            отслеживание <br />
+            присутствующих
+          </p>
+        </div>
+      </swiper-slide>
+
+      <swiper-slide>
+        <div class="slider-card" id="card-2">
+          <img src="/time.png" alt="" />
+          <h3>История занятий</h3>
+          <p>Материалы урока</p>
+        </div>
+      </swiper-slide>
+    </swiper-container>
+
+    <div class="container desktop">
+      <div class="row">
+        <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
+          <div class="opportunities__card" id="card-1">
+            <img src="/sms.png" alt="" />
+            <h3>Общение с классом</h3>
+            <p>
+              Видеосвязь <br />
+              Чат с классом
+            </p>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
+          <div class="opportunities__card" id="card-2">
+            <img src="/komp.png" alt="" />
+            <h3>Проведение занятий</h3>
+            <p>
+              Презентация файлов <br />
+              Демонстрация экрана
+            </p>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
+          <div class="opportunities__card" id="card-1">
+            <img src="/point.png" alt="" />
+            <h3>Посещаемость</h3>
+            <p>
+              Автоматическое <br />
+              отслеживание <br />
+              присутствующих
+            </p>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
+          <div class="opportunities__card" id="card-2">
+            <img src="/time.png" alt="" />
+            <h3>История занятий</h3>
+            <p>Материалы урока</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import VHeader from "/components/VHeader.vue";
+import VTutorial from "/components/VTutorial.vue";
+import VInfo from "/components/VInfo.vue";
+import VLessons from "/components/VLessons.vue";
+import VOpportunities from "/components/VOpportunities.vue";
+import VInstruction from "/components/VInstruction.vue";
+import VDistance__lessons from "/components/VDistance__lessons.vue";
+import VFooter from "/components/VFooter.vue";
+</script>
+
+<template>
+  <v-header />
+  <v-tutorial />
+  <v-info />
+  <v-lessons />
+  <v-opportunities />
+  <v-instruction />
+  <v-distance__lessons />
+  <v-footer />
+</template>
+
+<style scoped></style>
